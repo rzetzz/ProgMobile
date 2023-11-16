28d80e773f7464e8ff4d16cdd16b19222f4a7504
Fatkhur Rozi <30801605+rzetzz@users.noreply.github.com>
2023-11-16 11:34:32 +0700

    Add files via upload

---
+++ tugas/AuthController.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:get/get.dart';
+import 'package:modul_pertama/app/modules/tugas/tugas.model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class AuthController extends GetxController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  RxBool isLoading = false.obs;
+  Future<void> registerUser(String email, String password) async {
+    try {
+      isLoading.value = true;
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      Get.snackbar('Success', 'Registration successful',
+          backgroundColor: Colors.green);
+       //Navigate ke Login Page
+    } catch (error) {
+      Get.snackbar('Error', 'Registration failed: $error',
+          backgroundColor: Colors.red);
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}
